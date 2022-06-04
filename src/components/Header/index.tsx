8b97b75714039a584d2fe1a17afb36685c3a3590
Jirlan <jirlansouza08@gmaial.com>
2022-06-04 19:30:46 -0400

    Created the Header component and added the font link in _document.tsx file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <Link href="/">
+        <img src="logo.svg" alt="logo" />
+      </Link>
+    </header>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,18 @@
-import Document from 'next/document';
+import Document, { Head, Html } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+      </Html>
+    );
+  }
 }
