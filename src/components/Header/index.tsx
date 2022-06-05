00cf90335d22543b62ecd0b8b6c6e17bad36c9ae
Jirlan <jirlansouza08@gmaial.com>
2022-06-04 21:24:16 -0400

    Changed the home page and home styles scss file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 
+import commonStyles from '../../styles/common.module.scss';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
+      <div className={commonStyles.container}>
         <Link href="/">
           <img src="logo.svg" alt="logo" />
         </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,153 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <div className={commonStyles.container}>
+        <div className={styles.logo}>
+          <img src="logo.svg" alt="logo" />
+        </div>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <ul className={styles.posts}>
+          <li>
+            <h2>Teste</h2>
+            <p>
+              of letters, as opposed to using 'Content here, content here',
+              making it look like readable English. Many desktop publishing
+              packages and web page editors now use Lorem Ipsum as their default
+              model text,
+            </p>
+            <div>
+              <div>
+                <FiCalendar />
+                <span>04 Jun 2022</span>
+              </div>
+              <div>
+                <FiUser />
+                <span>Jirlan Souza</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <button type="button" className={styles.paginatorButton}>
+          Carregar mais posts
+        </button>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
